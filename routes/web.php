9b2b12cdab2fa2cf9e95f98739e997ff672fff10
Wu Chen-En <ap9941002@gmail.com>
2020-10-20 11:55:00 +0800

    第五週作業2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Route::get('/', function () {
+    return 'welcome';
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('r1',function(){
+    return redirect('r2');
+});
+Route::get('r2', function () {
+    return view('welcome');
+});
+Route::get('hello/{name}', function($name) {
+    return 'Hello, '.$name;
+});
+*/
+Route::get('hello/{name?}', function($name = 'Everybody') {
+return
+    'Hello, '.$name;
 });
